0a1446b0d794d28a83cfe2406b3fefd63d972dd6
Sean Pollock <seanpollock3344@gmail.com>
2017-04-08 16:29:55 -0500

    added conditional operator to transform by linecount

--- lines.js
+++ lines.js
@@ -1,9 +1,14 @@
 var split = require('split')
 var through2 = require('through2')
+var i = 0;
 process.stdin
   .pipe(split())
   .pipe(through2(function(line, _, next) {
-    console.log(line.toString())
+    var line = line.toString()
+    this.push(i % 2 === 0
+      ? line.toLowerCase() + '\n'
+      : line.toUpperCase() + '\n')
+    i++
     next()
   }))
   .pipe(process.stdout)
