1a196d134296ea6d1fdc48a666b304060ab3fbcc
Sean Pollock <seanpollock3344@gmail.com>
2017-04-08 21:35:23 -0500

    handler for post requests

--- response.js
+++ response.js
@@ -2,7 +2,11 @@ var http = require('http')
 var fs = require('fs')
 
 var server = http.createServer(function(req, res) {
-  
+  if (req.method === 'POST') {
+    req.pipe(createWriteStream('data.txt'))
+  }
+
+  res.end('beep boop\n')
 })
 
 server.listen(3000)
