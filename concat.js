50932f79a0d11dd2e0bd924fb7f0259a3d46d2a6
Sean Pollock <seanpollock3344@gmail.com>
2017-04-08 21:18:08 -0500

    Succesfully split and reversed string

--- concat.js
+++ concat.js
@@ -1,11 +1,16 @@
 var fs = require('fs')
 var concat = require('concat-stream')
 
-var readStream = fs.createReadStream(process.stdin)
-var concatStream = concat(gotFile)
+// var readStream = fs.createReadStream(process.stdin)
+// var concatStream = concat(gotFile)
+//
+// readStream.pipe(concatStream)
+//
+// function gotFile(fileBuffer) {
+//   console.log('fileBuffer', fileBuffer)
+// }
 
-readStream.pipe(concatStream)
 
-function gotFile(fileBuffer) {
-  console.log('fileBuffer', fileBuffer)
-}
+process.stdin.pipe(concat(function(data) {
+  console.log('data', data.toString().split('').reverse().join(''))
+}))
